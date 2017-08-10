8932cafba91f5a7af0ba1f21ae54355f7dc04d6f
guolonggang0914 <2363232934@qq.com>
2017-08-10 17:36:44 +0800

    郭龙刚提交积分xml页面

--- app/src/main/java/com/bway/two/view/fragment/RebateFragment.java
+++ app/src/main/java/com/bway/two/view/fragment/RebateFragment.java
@@ -1,8 +1,23 @@
 package com.bway.two.view.fragment;
 
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bway.two.R;
+
 /**
  * Created by Administrator on 2017/8/10 0010.
  */
 
-public class RebateFragment {
+public class RebateFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rebate,container,false);
+    }
 }
