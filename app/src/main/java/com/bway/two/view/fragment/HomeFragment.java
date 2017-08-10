c1fb2021652cb0dbba6f3dcc06fa6e1dce8e1a54
14844 <1484472688@qq.com>
2017-08-10 14:39:22 +0800

    luccc2

--- app/src/main/java/com/bway/two/view/fragment/HomeFragment.java
+++ app/src/main/java/com/bway/two/view/fragment/HomeFragment.java
@@ -1,6 +1,14 @@
 package com.bway.two.view.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bway.two.R;
 
 /**
  * Created by 卢程
@@ -9,4 +17,10 @@ import android.support.v4.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
 }
