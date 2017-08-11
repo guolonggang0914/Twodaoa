1258afc478cc81fe50ecb18b75e34c78a2ac04a7
lenovo <962229493@qq.com>
2017-08-11 22:26:31 +0800

    8.11号整合

--- app/src/main/java/com/bway/two/view/fragment/NearbyFragment.java
+++ app/src/main/java/com/bway/two/view/fragment/NearbyFragment.java
@@ -7,14 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.location.LocationClient;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
 import com.bway.two.R;
+import com.bway.two.model.map.MyLocationListener;
+import com.bway.two.utils.ImageShowUtils.YuanjiaoImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-
 /**
  * autor: 李金涛.
  * date:2017/8/10
@@ -23,23 +25,37 @@ import butterknife.Unbinder;
 
 public class NearbyFragment extends Fragment {
 
-//    @BindView(R.id.map_top)
-//    MapView mapTop;
     Unbinder unbinder;
+    @BindView(R.id.map_top)
+    MapView mapTop;
+    @BindView(R.id.image_ceshi)
+    YuanjiaoImageView imageCeshi;
     private View view;
-
-    String url = "https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=92e00c9b8f5494ee8722081f15ce87c3/29381f30e924b899c83ff41c6d061d950a7bf697.jpg";
-
+    public LocationClient mLocationClient = null;
+    public MyLocationListener myListener = new MyLocationListener();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         SDKInitializer.initialize(getActivity().getApplicationContext());
         view = inflater.inflate(R.layout.fragment_nearby, container, false);
         unbinder = ButterKnife.bind(this, view);
-//        YuanjiaoImageView imageView = view.findViewById(R.id.image_ceshi);
-//        Glide.with(getActivity()).load(url).into(imageView);
-//        ImageUtils.newInstance().disPlay(getActivity(),url,imageView);
+        //去掉百度地图的放大缩小图标
+        mapTop.showZoomControls(false);
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener( myListener );
+        mLocationClient.start();
+        //注册监听函数
         return view;
+    }
+
+    private void initView() {
+        initMap();
+
+    }
+
+    private void initMap() {
+
     }
 
     @Override
@@ -47,22 +63,28 @@ public class NearbyFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-//        mapTop.onDestroy();
+        if(mapTop!=null){
+            mapTop.onDestroy();
+        }
+
     }
+
     @Override
     public void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-//        mapTop.onResume();
+        mapTop.onResume();
     }
+
     @Override
     public void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-//        mapTop.onPause();
+        mapTop.onPause();
     }
 }
