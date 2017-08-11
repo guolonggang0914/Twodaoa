ea9c81b7e96f8daa827e0c031ef4104c44d2a27c
14844 <1484472688@qq.com>
2017-08-11 11:50:12 +0800

    首页完善一部分

--- app/src/main/java/com/bway/two/view/activity/MainActivity.java
+++ app/src/main/java/com/bway/two/view/activity/MainActivity.java
@@ -1,5 +1,9 @@
 package com.bway.two.view.activity;
 
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.bway.two.R;
 import com.bway.two.model.base.BaseActivity;
 
