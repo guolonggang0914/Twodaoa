ce2f6219e660f818c9b08d31df1653e6ed60f23c
lenovo <962229493@qq.com>
2017-08-11 11:56:09 +0800

    基本功能

--- app/src/main/java/com/bway/two/view/fragment/NearbyFragment.java
+++ app/src/main/java/com/bway/two/view/fragment/NearbyFragment.java
@@ -7,7 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.bway.two.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * autor: 李金涛.
@@ -17,9 +23,41 @@ import com.bway.two.R;
 
 public class NearbyFragment extends Fragment {
 
+    @BindView(R.id.map_top)
+    MapView mapTop;
+    Unbinder unbinder;
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nearby,container,false);
+        SDKInitializer.initialize(getActivity().getApplicationContext());
+        view = inflater.inflate(R.layout.fragment_nearby, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapTop.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mapTop.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mapTop.onPause();
     }
 }
